e718eaefa4ca83694f26d7084cc419b5b5dc994b
z5204771 <z5204771@ad.unsw.edu.au>
2019-02-02 14:40:51 +1100

    Added song "realtime" player

--- o2jam_utils/Helpers.cs
+++ o2jam_utils/Helpers.cs
@@ -6,11 +6,68 @@ using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace O2JamUtils
 {
     public class Helpers
     {
+        public static class HighResolutionDateTime
+        {
+            public static bool IsAvailable { get; private set; }
+
+            [DllImport("Kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+            private static extern void GetSystemTimePreciseAsFileTime(out long filetime);
+
+            public static DateTime UtcNow
+            {
+                get
+                {
+                    if (!IsAvailable)
+                    {
+                        throw new InvalidOperationException(
+                            "High resolution clock isn't available.");
+                    }
+
+                    long filetime;
+                    GetSystemTimePreciseAsFileTime(out filetime);
+
+                    return DateTime.FromFileTimeUtc(filetime);
+                }
+            }
+
+            public static long timenow
+            {
+                get
+                {
+                    if (!IsAvailable)
+                    {
+                        throw new InvalidOperationException(
+                            "High resolution clock isn't available.");
+                    }
+
+                    long filetime;
+                    GetSystemTimePreciseAsFileTime(out filetime);
+
+                    return filetime / 10000;
+                }
+            }
+
+            static HighResolutionDateTime()
+            {
+                try
+                {
+                    long filetime;
+                    GetSystemTimePreciseAsFileTime(out filetime);
+                    IsAvailable = true;
+                }
+                catch (EntryPointNotFoundException)
+                {
+                    // Not running Windows 8 or higher.
+                    IsAvailable = false;
+                }
+            }
+        }
         //safer memory mapping i guess...
         public static MemoryMappedFile MemFile(string path)
         {
